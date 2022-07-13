8e61cfdccb46d157de62f2b2db33898d20a19777
Ricardo Ramos <91920606+ricardoramosdev@users.noreply.github.com>
2022-07-13 20:36:11 -0300

    Comments (#4)
    * first commit
    
    * install fontAwesome,font family
    
    * react router iniciado
    
    * component home 
    
    * component movieList :search bar handleChange + basic structure movie cards
    
    * component movieList: fn filter movie
    
    * component catalog: styled andt install
    
    * component movieDescription
    
    * feature: favorite list
    
    * pivate route for favorites
    
    * comments section
    
    * replace localhost DB for heroku-backend

--- src/pages/MovieDescription/MovieDescription.jsx
+++ src/pages/MovieDescription/MovieDescription.jsx
@@ -1,57 +1,112 @@
-import { faHeart, faStarHalf } from "@fortawesome/free-regular-svg-icons";
+import { HeartFilled, HeartOutlined } from "@ant-design/icons";
 import { faArrowLeft, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Alert, Button, Modal } from "antd";
+import { modalGlobalConfig } from "antd/lib/modal/confirm";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { NavLink, useParams } from "react-router-dom";
+import { Navigate, NavLink, useNavigate, useParams } from "react-router-dom";
+import { useAuth } from "../../auth/useAuth";
 import "../../shared/Header/Header.scss";
+import { Comments } from "../Comment/Comments";
 import "./MovieDescription.scss";
-// import {Star, StarRating} from "./Star"
 const URL = "https://api.tvmaze.com/shows";
+const dbURL = process.env.REACT_APP_API_URL
 const json = require("../../database.json");
 
 export const MovieDescription = () => {
+  let navigate = useNavigate()
+
+  let auth =useAuth()
   let { movieID } = useParams();
   const [movie, setMovie] = useState({});
   const[image, setImage]=useState({})
   const[rating, setRating]=useState(0)
-  console.log(movieID);
-
+ 
   const displayMovie = async () => {
     try {
       const response = await axios.get(`${URL}/${movieID}`);
-      // const display = response.data.filter((el) => el.show.id === movieID);
-      // setMovie(...display);
-      console.log(response.data)
-      console.log(response.data)
+      
       setMovie(response.data)
       setImage(response.data.image)
-
       setRating(+response.data.rating.average)
 
-      // console.log(movie.show.name);
     } catch(error) {
-      console.log(json)
       const display = json.filter(el => el.show.id == movieID);
       let displayshow = {...display}
-      console.log(displayshow.show);
-      console.log(...display);
-      
       setMovie(...display);
-      console.log(movie)
-      console.log(image)
       console.log("error al conectarse", error);
     }
   };
-  console.log(rating)
+
+
+  //Favoritos
+  let favoritesMovies = auth.user?.favoriteMovie
+  const [favorite, setFavorite] = useState(true)
+  const [favoritesList, setFavoriteList] = useState([])
+    //compruevo si existe en la base de datos
+  let isOnDB = ()=>{
+    let onDB = favoritesMovies?.map(el=>el.id).find(id=>id==movieID)?true:false
+  setFavorite(onDB)}
+  
+  let user =JSON.parse(localStorage.getItem('currentUser'))
+  
+  //comprobar si esta en la lista de favoritos
+  const refreshFavorite = ()=>{
+  let isFavorite = user?.favoriteMovie.some(m=>m.id==(movieID))
+  //setear el estado de favorito
+  setFavorite(isFavorite)
+
+}
+//renderizado condicional del boton fav
+  const toogleFavorite =  ()=>{
+    
+   
+    //funcion toogle para cambiar estado
+    handleFavoritesDB()
+  }
+  const handleFavoritesDB = async ()=>{
+    setFavorite(!favorite)
+
+    if(favorite){
+      let updateUserFavorites = user.favoriteMovie.filter(el=>el.id!=movieID)
+      let removeFavorite = await axios.put(`${dbURL}/user/${auth.user._id}`,(user,{favoriteMovie:updateUserFavorites}))
+      localStorage.setItem("currentUser", JSON.stringify(removeFavorite.data))
+    }else{
+      try{
+      let updateUserFavorites = [...user.favoriteMovie,{...movie}]
+      let addFavorite = await axios.put(`${dbURL}/user/${auth.user._id}`, (user,{favoriteMovie:updateUserFavorites}));
+      localStorage.setItem("currentUser", JSON.stringify(addFavorite.data))
+      }catch(error){
+        console.log(error)
+      }
+    }
+  }
+  const toLogin =()=>{
+    Modal.info({
+      title:"Login Required",
+      content:"To add favorites please login",
+      okText:"Ok",
+      onOk:navigate("/login", { replace: false }),
+      closable:true
+      
+   
+  })}
+  //eliminar o agregar favorito en base de datos
+  useEffect(()=>{
+    isOnDB();
+  },[])
   useEffect(() => {
     displayMovie();
   },[]);
-
+  useEffect(()=>{
+    refreshFavorite();
+  },[])
   return (
     <>
       <header className="nav">
-        <NavLink to="/catalog">
+       
+        <NavLink to={-1} >
           <FontAwesomeIcon icon={faArrowLeft} />
         </NavLink>
       </header>
@@ -62,7 +117,6 @@ export const MovieDescription = () => {
             alt={movie.name}
             className="cover-img"
           />
-          {/* <img src={movie.image.medium} alt={movie.name} className="cover-img" /> */}
         <div className="star">
           {Array.from({ length: 5 }, (v, i) => (
           <span data-star-id={i + 1}  key={`star_${i + 1}`} >
@@ -73,7 +127,12 @@ export const MovieDescription = () => {
           
         </div>
         <div className="movie-data">
-          <h2>{movie.name}<a className="favorite" role={"button"}><FontAwesomeIcon icon={faHeart}/></a></h2>
+          <h2>{movie.name}<a 
+          className="favorite" 
+          role={"button"} 
+          onClick={auth.user?()=>toogleFavorite():()=>toLogin()} 
+          
+          >{favorite?<HeartFilled/>:<HeartOutlined/>}</a></h2>
           <ul className="movie-data-list">
             <li>Lenguaje: {movie.language}</li>
             <li>Género: {movie.genres}</li>
@@ -84,7 +143,7 @@ export const MovieDescription = () => {
           <h2>Sinopsis</h2>
           <div dangerouslySetInnerHTML={{__html:movie.summary}}></div>
         </div>
-        
+        <Comments movieID={{...movie}}/>
       </main>
     </>
   );
